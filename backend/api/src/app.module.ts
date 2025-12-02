d03c9581caddd3272526c34c865a808950428c4d
ksvcchh <mi.kasevich@gmail.com>
2025-12-02 23:21:02 +0100

    chore(BE): adds path to env file on API

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './validation/env-validation';
+import path from 'path';
+
+const env = process.env.NODE_ENV ?? 'development';
+
+const isTsRun = path.extname(__filename) === '.ts';
+
+const envFilePath = isTsRun
+  ? [path.join(__dirname, '..', '..', `.env.${env}`)]
+  : [path.join(__dirname, '..', '..', '..', `.env.${env}`)];
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, validate, envFilePath })],
   controllers: [AppController],
   providers: [AppService],
 })
