0743fb4902cbbdcd32cbb63bebd5eb6f0b410c35
Aleksandra Zegler <olazegler@gmail.com>
2025-12-08 15:29:46 +0100

    frontend(feat): add auth utilities

---
+++ frontend/lib/auth/logout.ts
@@ -0,0 +1,14 @@
+export async function logoutUser(): Promise<void> {
+  /*
+  const res = await fetch("https://api.shareway.app/api/v1/auth/logout", {
+    method: "POST",
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    throw new Error("Logout failed");
+  }
+  */
+
+  console.log("Mock logoutUser() called – później podłączymy API.");
+}
